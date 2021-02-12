eac92794543656887c64951643eacb4d31434a94
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-02-12 13:37:02 +0530

    file handling process from python

--- insert data into table.sql
+++ insert data into table.sql
@@ -4,3 +4,8 @@ e_id int, e_name varchar(50),e_gender varchar(10),
 e_age int, e_salary int, e_dept varchar(50),
 primary key(e_id)
 );
+
+insert into employee values(1,'shreyansh','m',21,2000,'data scientist');select * from employee;
+select e_age from employee;
+
+
