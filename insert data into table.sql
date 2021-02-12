3b96aa1f58a012dcfc36ed4959a3cf8f1a4a96fa
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-02-12 14:10:08 +0530

    insert data into table

--- insert data into table.sql
+++ insert data into table.sql
@@ -12,3 +12,4 @@ select e_age from employee;
 insert into employee values(2,'ravi','m',22,3000,'react module');
 insert into employee values(3,'rashika','f',23,4000,'data analyst');
 insert into employee values(4,'sneha','f',21,5000,'engineer');
+insert into employee values(2,'sneha','f',21,5000,'engineer');insert into employee values(2,'ravi','m',22,3000,'react module'); insert into employee values(3,'rashika','f',23,4000,'data analyst');
