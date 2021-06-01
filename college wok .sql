e39651d51fc052a2a4cfde033bf8ee3f6a2ae8c3
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-06-01 23:15:46 +0530

    college work has been added

--- college wok .sql
+++ college wok .sql
@@ -1,19 +1,27 @@
+show databases;
 use college;
 
 create table employee
 (
-  emp_id varchar(3) primary key ,
-  Ename varchar(25) not null check(Ename like 'E%'),
+  emp_id varchar(3) primary key check(emp_id like 'E%') ,
+  Ename varchar(25) not null ,
   DOB date not null,
   DOJ date not null, check(DOB<DOJ),
-  salary numeric(7) not null check(salary>0)
+  salary int  not null check(salary>0)
 );
 
 select*from employee;
 -- drop table employeees;
-insert into employee values('E06','Elish belli','2001-05-03','2015-06-09',61050);
+insert into employee values('E01','Elish belli','2001-05-03','2015-06-09',61050);
+insert into employee values('E02','shreyansh','1999-09-02','2012-03-19',45781);
+insert into employee values('E03','reyansh','1991-05-12','2015-06-20',60000);
+insert into employee values('E04','reya','1975-02-11','1995-11-18',54371);
+insert into employee values('E05','ansh','1978-04-12','1994-06-29',63456);
+insert into employee values('E06','shrey','1980-05-03','1998-1-19',34562);
+insert into employee values('E07','shreya','1997-11-13','2014-11-09',60050);
+insert into employee values('E08','shinu','1998-12-23','2016-06-19',69801);
 
-select*from employee where DOJ = '2019-03-09';
+select*from employee where DOJ = '2012-03-19';
 select emp_id,Ename,date_format(DOB,'%M-%D-%Y') from employee;
 select*from employee where(extract(year from DOJ))<2018;
 select*from employee where(extract(month from DOJ))=1;
