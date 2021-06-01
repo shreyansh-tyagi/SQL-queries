1e830f8e6affacb5c31882649a8a26c48c3578cf
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-06-01 23:15:10 +0530

    college work has been added

--- college wok .sql
+++ college wok .sql
@@ -1,6 +1,6 @@
-use yashdb;
+use college;
 
-create table employeees
+create table employee
 (
   emp_id varchar(3) primary key ,
   Ename varchar(25) not null check(Ename like 'E%'),
@@ -9,18 +9,18 @@ create table employeees
   salary numeric(7) not null check(salary>0)
 );
 
-select*from employeees;
+select*from employee;
 -- drop table employeees;
-insert into employeees values('E06','Elish belli','2001-05-03','2015-06-09',61050);
+insert into employee values('E06','Elish belli','2001-05-03','2015-06-09',61050);
 
-select*from employeees where DOJ = '2019-03-09';
-select emp_id,Ename,date_format(DOB,'%M-%D-%Y') from employeees;
-select*from employeees where(extract(year from DOJ))<2018;
-select*from employeees where(extract(month from DOJ))=1;
-select*from employeees where(extract(year from DOJ))=2019;
-select *from employeees order by(DOJ) desc;
-select Ename,datediff('2021-06-01',DOJ) as 'DAY_OF_EXPERIENCE' from employeees;
-select*from employeees where mod(salary,2)!=0;
-select*from employeees where emp_ID = 'E01' and extract(year from DOJ) =1991;
-select*from employeees where extract(month from DOJ)!=2;
-select concat('HAPPY BIRTHDAY TO ',Ename) from employeees where extract(DAY from DOB)= 1 and extract(month from DOB) = 2; 
+select*from employee where DOJ = '2019-03-09';
+select emp_id,Ename,date_format(DOB,'%M-%D-%Y') from employee;
+select*from employee where(extract(year from DOJ))<2018;
+select*from employee where(extract(month from DOJ))=1;
+select*from employee where(extract(year from DOJ))=2019;
+select *from employee order by(DOJ) desc;
+select Ename,datediff('2021-06-01',DOJ) as 'DAY_OF_EXPERIENCE' from employee;
+select*from employee where mod(salary,2)!=0;
+select*from employee where emp_ID = 'E01' and extract(year from DOJ) =1991;
+select*from employee where extract(month from DOJ)!=2;
+select concat('HAPPY BIRTHDAY TO ',Ename) from employee where extract(DAY from DOB)= 1 and extract(month from DOB) = 2; 
