44d588964dc2995fa2f879dc265eb3b72d97a7a1
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-06-02 00:52:17 +0530

    college work has been added

--- college wok .sql
+++ college wok .sql
@@ -32,8 +32,10 @@ select * from employee where day(doj)>=15;
 select * from employee where day(doj)>=15 and year(doj)>=2015;
 select *from employee order by(DOJ) desc;
 select Ename,datediff('2021-06-01',DOJ) as 'DAY_OF_EXPERIENCE' from employee;
-select Ename,datediff('2021-06-01',DOJ) as 'year_OF_EXPERIENCE' from employee;
 select*from employee where mod(salary,2)!=0;
+select*from employee where (salary%2)!=0;
+
 select*from employee where emp_ID = 'E01' and extract(year from DOJ) =1991;
+select
 select*from employee where extract(month from DOJ)!=2;
 select concat('HAPPY BIRTHDAY TO ',Ename) from employee where extract(DAY from DOB)= 1 and extract(month from DOB) = 2; 
